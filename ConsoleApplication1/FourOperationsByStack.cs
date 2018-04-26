46c7386df27383b43633a62e47e42f25b3310e74
yinbi <8634948@qq.com>
2018-04-27 01:02:06 +0800

    add

---
+++ ConsoleApplication1/FourOperationsByStack.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApplication1
+{
+    public class FourOperationsByStack
+    {
+        public string expression;
+        public FourOperationsByStack()
+        {
+
+        }
+        public FourOperationsByStack(string expression)
+        {
+            this.expression = expression;
+        }
+        //提取数字与操作符
+
+        //中缀表达式转后缀表达式
+        //计算
+    }
+}
